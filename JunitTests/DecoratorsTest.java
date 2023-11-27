523c557d750eed74851627d75972131fbae4d063
Lucky13forever <emanuel.lugojan02@e-uvt.ro>
2023-11-27 23:13:28 +0200

    finish_economy_decorator

---
+++ JunitTests/DecoratorsTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import ro.uvt.dp.account.Account;
+import ro.uvt.dp.account.AccountEUR;
+import ro.uvt.dp.account.EconomyAccountDecorator;
+import ro.uvt.dp.exceptions.AmountException;
+import ro.uvt.dp.logger.Logger;
+
+import static org.junit.Assert.*;
+
+public class DecoratorsTest {
+
+
+    @Test
+    public void EconomyDecorator() throws AmountException {
+        // Get two instances of the logger
+        Account myAccount = new AccountEUR("RO123456789", 1000);
+        EconomyAccountDecorator myEconomyAccount = new EconomyAccountDecorator(myAccount, 30);
+
+        assertEquals(1000, myEconomyAccount.get_roi(), 0);
+    }
+}
--- src/ro/uvt/dp/account/EconomyAccountDecorator.java
+++ src/ro/uvt/dp/account/EconomyAccountDecorator.java
@@ -4,18 +4,26 @@ import ro.uvt.dp.exceptions.AmountException;
 
 public class EconomyAccountDecorator extends AccountDecorator {
 
-    private double discountRate;
-
-    public EconomyAccountDecorator(Account account, double discountRate) throws AmountException {
+    private int months;
+    private double interestRate;
+    public EconomyAccountDecorator(Account account, int months) throws AmountException {
         super(account);
-        this.discountRate = discountRate;
+        this.months = months;
+        // 1 month 0.2% , 3 months - 1%, 6 months - 3%, 12 months - 7%
+        int for_12 = months / 12;
+        int left = months - for_12 * 12;
+        int for_6 = (months - left) / 6;
+        left = left - for_6 * 6;
+        int for_3 = left / 3;
+        left = left - for_3 * 3;
+        int for_1 = left;
+        interestRate = for_12 * 7 + for_6 * 3 + for_3 * 1 + for_1 * 0.2;
     }
 
     @Override
     public double getTotalAmount() {
         // Applying discount rate to the total amount
-        double baseAmount = super.getTotalAmount();
-        return baseAmount * (1 - discountRate);
+        return super.getTotalAmount();
     }
 
     @Override
@@ -28,5 +36,9 @@ public class EconomyAccountDecorator extends AccountDecorator {
 
     }
 
-    // Additional methods related to economy accounts can be added here
+    public int get_roi() {
+        // Calculate the return on investment
+        return (int) (getTotalAmount() * (interestRate / 100));
+    }
+
 }
